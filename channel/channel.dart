6503844941e7af8a30a53aceb4f06eb21f741105
Falways <1914783590@qq.com>
2019-06-17 11:29:55 +0800

    up

--- channel/channel.dart
+++ channel/channel.dart
@@ -1,27 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class PlatformChannel extends StatefulWidget {
+  @override
   _PlatformChannelState createState() => _PlatformChannelState();
 }
 
 class _PlatformChannelState extends State<PlatformChannel> {
-  // 通过key找到对应的method, 每个Channel在创建时必须指定一个独一无二的name, 被动模式
   static const MethodChannel methodChannel = MethodChannel('xuhang.channel/battry');
-  // 主动模式，可以自动把值推送过来
   static const EventChannel eventChannel = EventChannel('xuhang.chanel/charging');
 
   String _batteryLevel = 'Battery level: unknown.';
   String _chargingStatus = 'Battery status: unknown.';
 
-  // 使用async控制程序同步, --未来、前途
   Future<void> _getBatteryLevel() async {
     String batteryLevel;
-    try{
+    try {
       final int result = await methodChannel.invokeMethod('getBatteryLevel');
       batteryLevel = 'Battery level: $result%.';
-    }on PlatformException{
+    } on PlatformException {
       batteryLevel = 'Failed to get battery level.';
     }
     setState(() {
@@ -31,18 +34,18 @@ class _PlatformChannelState extends State<PlatformChannel> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    eventChannel.receiveBroadcastStream().listen(_onEvent,onError:_onError);
+    eventChannel.receiveBroadcastStream().listen(_onEvent, onError: _onError);
   }
 
-  void _onEvent(Object event){
+  void _onEvent(Object event) {
     setState(() {
-      _chargingStatus = "Battery status: ${event == 'charging' ? '' : 'dis'}charging.";
+      _chargingStatus =
+      "Battery status: ${event == 'charging' ? '' : 'dis'}charging.";
     });
   }
 
-  void _onError() {
+  void _onError(Object error) {
     setState(() {
       _chargingStatus = 'Battery status: unknown.';
     });
@@ -57,23 +60,23 @@ class _PlatformChannelState extends State<PlatformChannel> {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(_batteryLevel,key: const Key('Battery level label')),
+              Text(_batteryLevel, key: const Key('Battery level label')),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: RaisedButton(
-                    child: const Text('Refresh'),
-                    onPressed: _getBatteryLevel,
+                  child: const Text('Refresh'),
+                  onPressed: _getBatteryLevel,
                 ),
-              )
+              ),
             ],
           ),
-          Text(_chargingStatus)
+          Text(_chargingStatus),
         ],
       ),
     );
   }
 }
 
-void main(){
+void main() {
   runApp(MaterialApp(home: PlatformChannel()));
 }

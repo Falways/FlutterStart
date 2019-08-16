e906cb879ab4333e37007404899aa4b25e686455
Falways <1914783590@qq.com>
2019-08-16 13:53:58 +0800

    add more interesting things

---
+++ contrast/lib/HomePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("aaaaaaaaaaaaaaaaa");
+    return Center(
+      child: Text("首页"),
+    );
+  }
+}
